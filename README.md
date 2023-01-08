9fb49887b0c2ecb134d1e5c49272a5977152a2e4
Bob Jarvis <bob@zeal.co>
2023-01-08 15:24:01 +1030

    using python-frontmatter, outlined next steps

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This website is built with Hugo. Hugo is less of a mess than Gatsby but Go is wi
 
 The content of this website is pulled from my private obsidian vault includes project documentation, research and development notes, literature notes, and pieces of writing.  The vault is iterated over and parsed in to hugo formatted markdown by way of a python script, and copied into the content folder of this repository. In this way the website is a public facing version of ZEAL CO creative output and research.
 
+## Python Script Requires:
+
+```
+pip install python-frontmatter
+```
--- obsidian2hugo.py
+++ obsidian2hugo.py
@@ -1,19 +1,43 @@
 import os
 import sys
 import shutil
+import frontmatter
+
+# Function to check if markdown file is has publish=True in yaml frontmatter
+def has_publish_frontmatter(markdown_file):
+    with open(markdown_file, "r") as f:
+
+        thismdfile = frontmatter.load(f)
+
+        if 'publish' in thismdfile and thismdfile['publish'] == 'y':
+            return True
+
+    # Return False if the frontmatter does not contain the "publish" property or if it is not set to True
+    return False
+
 
 # Function to recursively iterate over all files in a directory
 def iterate_files(src_path, dst_path):
     # Iterate over all files in the source path
     for name in os.listdir(src_path):
-        # Get the full path of the file or directory
-        src_full_path = os.path.join(src_path, name)
-        # Get the corresponding path in the destination directory
-        dst_full_path = os.path.join(dst_path, name)
+
+        # TODO for project files, check markdown file for publish=y
+        # copy entire folders for project including assets etc
+
         # If the file is a markdown file, copy it to the destination path
         if name.endswith(".md"):
-            shutil.copy(src_full_path, dst_full_path)
+            # Get the full path of the file or directory
+            src_full_path = os.path.join(src_path, name)
 
+            # Check if markdown file has publish flag
+            if has_publish_frontmatter(src_full_path):
+                # Get the corresponding path in the destination directory
+                dst_full_path = os.path.join(dst_path, name)
+                shutil.copy(src_full_path, dst_full_path)
+
+
+
+# MAIN ----------------------------------------------------
 # Get the path from the command line arguments
 src_path = sys.argv[1]
 dst_path = sys.argv[2]
@@ -25,3 +49,6 @@ os.makedirs(notebook_path, exist_ok=True)
 
 # Iterate over all files in the root of the source path
 iterate_files(src_path, notebook_path)
+
+# TODO
+# process copied markdown files in place into hugo MD
